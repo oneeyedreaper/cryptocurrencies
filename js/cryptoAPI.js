b67368aed43cb79165fe42cde09ddfb9db93155c
Shadow Monarch <53879034+oneeyedreaper@users.noreply.github.com>
2021-08-19 05:01:33 +0530

    Update cryptoAPI.js

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,5 +1,5 @@
 class Cryptoshit{
-    apikey='1d0728b5-1d5a-4b15-91a4-b38a218b1b46';
+    apikey='your api key bois';
     async fetchshit(){
          const poop = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+this.apikey+'&sort=cmc_rank&limit=20')
          const poopy=await poop.json();

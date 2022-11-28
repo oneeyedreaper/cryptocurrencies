8624830eb253e326983b1e7d20b5a91d64126db4
Shadow Monarch <53879034+oneeyedreaper@users.noreply.github.com>
2022-11-28 17:07:02 +0530

    api key

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,16 +1,24 @@
-class Cryptoshit{
-    apikey='your api key bois';
-    async fetchshit(){
-         const poop = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+this.apikey+'&sort=cmc_rank&limit=20')
-         const poopy=await poop.json();
-            return poopy;
-    }
-    async patalagao(cryptocurrency,currency){
-       const shit= await fetch('https://pro-api.coinmarketcap.com/v1/tools/price-conversion?CMC_PRO_API_KEY='+this.apikey+'&amount=1&id='+cryptocurrency+'&convert='+currency)
-        const quote=await shit.json();
-        return quote;
-
-    }
-    
-      
-} 
+class Cryptoshit {
+	apikey = "1d0728b5-1d5a-4b15-91a4-b38a218b1b46";
+	async fetchshit() {
+		const poop = await fetch(
+			"https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=" +
+				this.apikey +
+				"&sort=cmc_rank&limit=20"
+		);
+		const poopy = await poop.json();
+		return poopy;
+	}
+	async patalagao(cryptocurrency, currency) {
+		const shit = await fetch(
+			"https://pro-api.coinmarketcap.com/v1/tools/price-conversion?CMC_PRO_API_KEY=" +
+				this.apikey +
+				"&amount=1&id=" +
+				cryptocurrency +
+				"&convert=" +
+				currency
+		);
+		const quote = await shit.json();
+		return quote;
+	}
+}
